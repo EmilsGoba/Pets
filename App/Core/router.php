fa43bdaf256b6ac89c6ba204159e098af9f09759
EmilsGoba <ipa23.e.goba@vtdt.edu.lv>
2024-12-09 12:58:57 +0200

    duiab
    udha

--- App/Core/router.php
+++ App/Core/router.php
@@ -1,8 +1,9 @@
 <?php
+$routes = require "../App/routes.php";
+
 $url_array = parse_url($_SERVER["REQUEST_URI"]);
 $url = $url_array["path"];
 
-$routes = require "../App/routes.php";
 
 if (array_key_exists($url, $routes)) {
     require $routes[$url];
--- App/config.php
+++ App/config.php
@@ -1,29 +1,8 @@
 <?php
-// Establish database connection
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $config = [
-        "host" => "localhost",
-        "dbname" => "pets",
-        "user" => "root",
-        "password" => "",
-        "charset" => "utf8mb4"
-    ];
-
-    try {
-        $dsn = "mysql:host={$config['host']};dbname={$config['dbname']};charset={$config['charset']}";
-        $pdo = new PDO($dsn, $config['user'], $config['password']);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        // Retrieve POST data
-        $username = $_POST['username'];
-        $solve_time = (int) $_POST['solve_time'];
-
-        // Insert the score into the database
-        $stmt = $pdo->prepare("INSERT INTO users (username, solve_time) VALUES (:username, :solve_time)");
-        $stmt->execute([':username' => $username, ':solve_time' => $solve_time]);
-
-        echo json_encode(["success" => true, "message" => "Score submitted successfully!"]);
-    } catch (PDOException $e) {
-        echo json_encode(["success" => false, "message" => "Database error: " . $e->getMessage()]);
-    }
-}
+return [
+  "host" => "localhost",
+  "dbname" => "root",
+  "user" => "root",
+  "password" => "",
+];
+?> 
